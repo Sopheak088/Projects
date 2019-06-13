eb9d2040a5d621b036e1194f1d9e0abfb9ab3120
Sopheak Davin <Sopheak Davin@DARLING-PC>
2019-06-13 11:21:17 +0700

    Merge Project testing

---
+++ Main/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly string _solution = ConfigurationManager.AppSettings["Solution"];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            //MessageBox.Show(_solution + ConfigurationManager.AppSettings["Inventory"]);
+            Process.Start(_solution + ConfigurationManager.AppSettings["Notice"]);
+            this.Close();
+        }
+    }
+}
--- Main/Program.cs
+++ Main/Program.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Inventory.Employee.View;
 
-namespace Inventory
+namespace Main
 {
-    public static class Program
+    static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static void Main()
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmEmployee());
+            Application.Run(new Form1());
         }
     }
 }
